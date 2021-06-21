14ec54cd5c7da31c02024c429060022e03e7291e
Lino437 <4804339450@knights.ucf.edu>
2021-06-21 14:49:09 -0400

    update #3

--- src/test/java/ex46/WordFrequencyFinderTest.java
+++ src/test/java/ex46/WordFrequencyFinderTest.java
@@ -7,8 +7,25 @@
 
 package ex46;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordFrequencyFinderTest {
+    @Test
+    @DisplayName("checking asterisk function with 0 as value")
+    void asteriskFunction0IntValue(){
+        int num = 0;
+        assertEquals("",WordFrequencyFinder.asteriskCount(num));
+    }
+    @Test
+    @DisplayName("checking asterisk function with 5 as value")
+    void asteriskFunction5IntValue(){
+        int num = 5;
+        assertEquals("*****",WordFrequencyFinder.asteriskCount(num));
+    }
+
+
 
 }
